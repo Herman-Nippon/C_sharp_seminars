5304fe2ae2c593c74364cb69dc0a636a8cf7aca5
Herman <verypuzikov2004@gmail.com>
2023-05-19 16:04:52 +0500

    add task05

---
+++ task05/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 1)
+    Console.WriteLine("Некорректный ввод.");
+else
+{
+    int i = -number;
+    while (i <= number)
+    {
+        Console.Write($"{i} ");
+        ++i;
+    }
+}
