ab4e119665e74841aa18953647e0d8059fd112cf
Herman <verypuzikov2004@gmail.com>
2023-05-20 09:44:47 +0500

    update task03 with switch case

--- task03/Program.cs
+++ task03/Program.cs
@@ -1,35 +1,30 @@
-﻿Console.Write("Введите число, соответствующее дню недели: ");
+﻿Console.Write("Enter a number, corresponding to a weekday: ");
 string num = Console.ReadLine()!;
 
-if (num == "1")
+switch(num)
 {
-    Console.WriteLine("Понедельник");
-}
-else if (num == "2")
-{
-    Console.WriteLine("Вторник");
-}
-else if (num == "3")
-{
-    Console.WriteLine("Среда");
-}
-else if (num == "4")
-{
-    Console.WriteLine("Четверг");
-}
-else if (num == "5")
-{
-    Console.WriteLine("Пятница");
-}
-else if (num == "6")
-{
-    Console.WriteLine("Суббота");
-}
-else if (num == "7")
-{
-    Console.WriteLine("Воскресенье");
-}
-else
-{
-    Console.WriteLine("Некорректный ввод");
+    case "1":
+        Console.WriteLine("Monday");
+        break;
+    case "2":
+        Console.WriteLine("Tuesday");
+        break;
+    case "3":
+        Console.WriteLine("Wednesday");
+        break;
+    case "4":
+        Console.WriteLine("Thursday");
+        break;
+    case "5":
+        Console.WriteLine("Friday");
+        break;
+    case "6":
+        Console.WriteLine("Saturday");
+        break;
+    case "7":
+        Console.WriteLine("Sunday");
+        break;
+    default:
+        Console.WriteLine("Incorrect input.");
+        break;
 }
