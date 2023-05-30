d8fded9683cd5b02c66315aedb2cefdfea439541
Herman <verypuzikov2004@gmail.com>
2023-05-30 12:57:39 +0500

    add task26

---
+++ task26/Program.cs
@@ -0,0 +1,29 @@
+﻿int DigitsInNumber(int num)
+{
+    int digits = 0;
+
+    if (num == 0) return 1;
+    else if (num > 0)
+    {
+        while (num > 0)
+        {
+            digits++;
+            num /= 10;
+        }
+        
+    }
+    else if (num < 0)
+    {
+        while (num < 0)
+        {
+            digits++;
+            num /= 10;
+        }
+    }
+    return digits;
+}
+
+Console.Write("Enter a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"{number} contains {DigitsInNumber(number)} digits");
