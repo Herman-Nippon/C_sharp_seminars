56a46ad4fa40de9520a5199996c139d3e2928a4c
Herman <verypuzikov2004@gmail.com>
2023-06-06 12:33:47 +0500

    add task39

---
+++ task39/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 39: Напишите программу, которая перевернёт
+// одномерный массив 
+
+int[] CreateFillArray(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rand = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    int length = arr.Length;
+    Console.Write("[");
+    for (int i = 0; i < length; i++)
+    {
+        if (length - i > 1)
+            Console.Write($"{arr[i]}, ");
+        else
+            Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+void ReverseArray(int[] arr)
+{
+    int len = arr.Length;
+    for (int i = 0; i < len / 2; i++)
+    {
+        int temp = arr[i];
+        arr[i] = arr[len - 1 - i];
+        arr[len - 1 - i] = temp;
+    }
+}
+
+int[] array = CreateFillArray(5, 1, 10);
+PrintArray(array);
+ReverseArray(array);
+PrintArray(array);
+
