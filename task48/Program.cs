9b8260498b751df1ce32344275b9d320d26b66aa
Herman <verypuzikov2004@gmail.com>
2023-06-09 12:56:51 +0500

    add task48

---
+++ task48/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 48: Задайте двумерный массив размера m на n,
+// каждый элемент в массиве находится по формуле: Amn = m+n.
+// Выведите полученный массив на экран.
+
+
+int[,] CreateFillMatrix(int m, int n)
+{
+    int[,] arr = new int[m,n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            arr[i,j] = i + j;
+        }
+    }
+    return arr;
+}
+
+void PrintMatrix(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i,j], 2}");
+        }
+
+        Console.WriteLine();
+    }
+}
+
+int[,] array = CreateFillMatrix(3, 4);
+PrintMatrix(array);
