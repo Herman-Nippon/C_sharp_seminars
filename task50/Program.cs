35a89a327a5811446ec85d60cace27ab42747e87
Herman <verypuzikov2004@gmail.com>
2023-06-10 10:39:35 +0500

    update task50 -- better logic and indexing from 0

--- task50/Program.cs
+++ task50/Program.cs
@@ -31,20 +31,18 @@ void PrintMatrix(int[,] arr)
 
 int[] PromptRowColumn()
 {
-    int[] rowCol = new int[2];
-    Console.WriteLine("Indexing starts from 1"); // Indexing starts from 1 for the more intuitive prompt
+    int[] rowCol = new int[2]; 
     Console.Write("Enter the row: ");
     rowCol[0] = Convert.ToInt32(Console.ReadLine());
     Console.Write("Enter the column: ");
     rowCol[1] = Convert.ToInt32(Console.ReadLine());
 
-    if (rowCol[0] < 1 || rowCol[1] < 1) return null!;
     return rowCol;
 }
 
 bool IsInArray(int row, int column, int[,] arr)
 {
-    if (row > arr.GetLength(0) || column > arr.GetLength(1)) return false;
+    if (row >= arr.GetLength(0) || row < 0 || column >= arr.GetLength(1) || column < 0) return false;
     return true;
 }
 
@@ -56,7 +54,7 @@ int[] rowColumn = PromptRowColumn();
 Console.WriteLine();
 PrintMatrix(array);
 
-if (rowColumn == null || !IsInArray(rowColumn[0], rowColumn[1], array))
-    Console.WriteLine("There's no such index in the array");
+if (IsInArray(rowColumn[0], rowColumn[1], array))
+    Console.WriteLine($"The value of [{rowColumn[0]}, {rowColumn[1]}] is {array[rowColumn[0],rowColumn[1]]}");
 else
-    Console.WriteLine($"The value of [{rowColumn[0]}, {rowColumn[1]}] is {array[rowColumn[0]-1,rowColumn[1]-1]}");
+    Console.WriteLine("There's no such index in the array");
